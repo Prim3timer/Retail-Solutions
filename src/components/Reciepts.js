b15f6201db1377f6b67977a9f127331989801ad0
Amalu Ekwelie <67154046+Prim3timer@users.noreply.github.com>
2026-02-01 13:27:43 -0500

    for Naomi Succes

--- src/components/Reciepts.js
+++ src/components/Reciepts.js
@@ -1,7 +1,7 @@
-import initialState from "../store"
-import { useEffect, useContext, useReducer, useState } from "react"
-import reducer from "../reducer"
-import axios from "../app/api/axios"
+import initialState from "../store";
+import { useEffect, useContext, useReducer, useState } from "react";
+import reducer from "../reducer";
+import axios from "../app/api/axios";
 import AuthContext from "../context/authProvider";
 import useAuth from "../hooks/useAuth";
 import useRefreshToken from "../hooks/useRefreshToken";
@@ -11,288 +11,282 @@ import { FaTrashAlt } from "react-icons/fa";
 import Unauthorized from "./Unauthorized";
 import { Link } from "react-router-dom";
 
-const Shopping = ()=> {
-const [state, dispatch] = useReducer(reducer, initialState)
+const Shopping = () => {
+  const [state, dispatch] = useReducer(reducer, initialState);
 
-const [oneId, setOneId] = useState('')
-const { auth, setAuth } = useAuth();
-const [currentUser, setCurrentUser] = useState('')
-const {atHome, getUsers, currentUsers, setIsRotated, oneShow, bizName} = useContext(AuthContext)
-console.log(state.atHome)
-const refresh = useRefreshToken()
-const getItems = async ()=> {
+  const [oneId, setOneId] = useState("");
+  const { auth, setAuth } = useAuth();
+  const [currentUser, setCurrentUser] = useState("");
+  const {
+    atHome,
+    getUsers,
+    currentUsers,
+    setIsRotated,
+    oneShow,
+    bizName,
+    currency,
+  } = useContext(AuthContext);
+  console.log(state.atHome);
+  const refresh = useRefreshToken();
+  const getItems = async () => {
     // const authUsers = await refresh()
     // console.log(authUsers)
-    console.log({currentUsers})
-    setOneId(auth.picker)
-    console.log(auth.picker)
+    console.log({ currentUsers });
+    setOneId(auth.picker);
+    console.log(auth.picker);
     // setAuth(authUsers)
     // const person = users.find((user) => user._id === pickerChecker)
     // auth.picker3 = state.id
     try {
+      console.log("picker3 is : ", auth.picker3);
+      console.log("picker is: ", auth.picker);
+      //   const gog =  await axios.get('/users')
 
-        console.log('picker3 is : ', auth.picker3)
-        console.log('picker is: ', auth.picker)
-            //   const gog =  await axios.get('/users')
-    
-            // setCurrentUser(person)
-    
-            const response = await axios.get('/transactions')
-            if (response){
-        
-             
-                const cashierTrans = response.data.filter((item) => item.cashierID === localStorage.getItem('memUser'))
-                      const person = auth.user && auth.users.find((person) => person._id == localStorage.getItem('memUser'))
-                      console.log(person)
-                      if (person ){
-                          setCurrentUser(person)
+      // setCurrentUser(person)
 
-                      }
-                      console.log(currentUser)
-                console.log(cashierTrans)
-                // dispatch({type: 'getNames', payload: response.data})
-                cashierTrans.reverse()
-                dispatch({type: 'getNames', payload: cashierTrans})
-    
-                const filterate = cashierTrans.filter((inner)=> inner.date.substring(0, 10).includes(state.search))
-                console.log(filterate)
-            
-                
-                console.log(state.getNames)
-                dispatch({type: 'getNames', 
-                    payload: filterate})
-                    
-                }
-    }catch (error) {
-        console.log(error)
+      const response = await axios.get("/transactions");
+      if (response) {
+        const cashierTrans = response.data.filter(
+          (item) => item.cashierID === localStorage.getItem("memUser"),
+        );
+        const person =
+          auth.user &&
+          auth.users.find(
+            (person) => person._id == localStorage.getItem("memUser"),
+          );
+        console.log(person);
+        if (person) {
+          setCurrentUser(person);
+        }
+        console.log(currentUser);
+        console.log(cashierTrans);
+        // dispatch({type: 'getNames', payload: response.data})
+        cashierTrans.reverse();
+        dispatch({ type: "getNames", payload: cashierTrans });
+
+        const filterate = cashierTrans.filter((inner) =>
+          inner.date.substring(0, 10).includes(state.search),
+        );
+        console.log(filterate);
+
+        console.log(state.getNames);
+        dispatch({ type: "getNames", payload: filterate });
+      }
+    } catch (error) {
+      console.log(error);
     }
-   
-    
-}
-console.log(currentUser)
+  };
+  console.log(currentUser);
 
-const assertain = (id) => {
-    if (auth.roles.includes(5150)){
-        console.log("deleted")
-        
-        dispatch({type: 'cancel', payload: true})
-        console.log(state.cancel)
-        dispatch({type: 'id', payload: id})
-        const getItem = state.getNames && state.getNames.find((item)=> item._id === id)
-        dispatch({type: 'inItem', payload: getItem})
-        console.log(getItem)
+  const assertain = (id) => {
+    if (auth.roles.includes(5150)) {
+      console.log("deleted");
+
+      dispatch({ type: "cancel", payload: true });
+      console.log(state.cancel);
+      dispatch({ type: "id", payload: id });
+      const getItem =
+        state.getNames && state.getNames.find((item) => item._id === id);
+      dispatch({ type: "inItem", payload: getItem });
+      console.log(getItem);
+    } else {
+      dispatch({ type: "isMatched", payload: true });
     }
-    else {
-        dispatch({type: 'isMatched', payload: true})
-    }
-}
+  };
 
-
-const handleRemove = async ()=> {
-    dispatch({type: 'cancel', payload: false})
-    const response = await axios.delete(`/transactions/${state.id}`)
+  const handleRemove = async () => {
+    dispatch({ type: "cancel", payload: false });
+    const response = await axios.delete(`/transactions/${state.id}`);
     // const newGraw = state.items && state.items.filter((item)=> item._id !== state.id)
-    console.log(response)
+    console.log(response);
 
-
-    // e.preventDefault()     
+    // e.preventDefault()
     // removeInventory(id)
-        // await axios.delete(`/transactions/${id}`)
-        
-        const newGraw = state.getNames.filter((item)=> item._id !== state.id)
-        console.log('removed')
-    dispatch({type: 'getNames', payload: newGraw})
-}
+    // await axios.delete(`/transactions/${id}`)
 
+    const newGraw = state.getNames.filter((item) => item._id !== state.id);
+    console.log("removed");
+    dispatch({ type: "getNames", payload: newGraw });
+  };
 
-
-const remainDelete = ()=> {
-    // this condition statement is to enable the removal of the confirm window once any part of the 
+  const remainDelete = () => {
+    // this condition statement is to enable the removal of the confirm window once any part of the
     // page is touched.
-    if (state.cancel){
-
-        dispatch({type: 'cancel', payload: false})
+    if (state.cancel) {
+      dispatch({ type: "cancel", payload: false });
     }
-
 
     // if (state.isEdit){
-        
+
     //     dispatch({type: 'isEdit', payload: false})
     // }
-}
-const generalRemain = () => {
-    if (state.isMatched) dispatch({type: 'isMatched', payload: false})
-        setIsRotated(false)
+  };
+  const generalRemain = () => {
+    if (state.isMatched) dispatch({ type: "isMatched", payload: false });
+    setIsRotated(false);
+  };
 
- } 
+  useEffect(() => {
+    getItems();
+  }, [state.search]);
 
-useEffect(()=> {
-    getItems()
-}, [state.search])
+  // useEffect(()=> {
 
-// useEffect(()=> {
+  // // if (sessionId){
+  //     refresh()
 
-// // if (sessionId){
-//     refresh()
+  // // }
+  // }, [ ])
 
-// // }
-// }, [ ])
-
-function numberWithCommas(x) {
+  function numberWithCommas(x) {
     return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-}
-    return (
-        !currentUser ? <h2
-        
-        className="receipts"
-        >Loading...</h2> : <div
-        className="receipts"
-        
-        onClick={generalRemain}
-        
-        // onClick={remainDelete}
+  }
+  return !currentUser ? (
+    <h2 className="receipts">Loading...</h2>
+  ) : (
+    <div
+      className="receipts"
+      onClick={generalRemain}
+
+      // onClick={remainDelete}
+    >
+      <h2
+        style={{
+          margin: "1rem 0",
+          // color: 'darkslateblue'
+        }}
+      >
+        {currentUser &&
+          `${currentUser.username}${currentUser.username ? "'s" : ""}`}{" "}
+        Reciepts ({state.getNames.length})
+      </h2>
+      {/* <Link to="one-receipt"> */}
+      <article id="form-cont">
+        <form
+          className="receipt-search-form"
+          onSubmit={(e) => e.preventDefault()}
         >
-         <h2
-            style={{
-                margin: '1rem 0' ,
-                // color: 'darkslateblue'    
-            }}
-           
-            >{ currentUser && `${currentUser.username}${currentUser.username ?"'s" : ''}`} Reciepts ({state.getNames.length})</h2>
-{/* <Link to="one-receipt"> */}
-<article id="form-cont">
-            <form  className="receipt-search-form"   onSubmit={(e)=> e.preventDefault()}>
-        <input 
-        id="receipt-search"
-        type="text"
-        role="searchbox" 
-        placeholder="Search by date"
-        value={state.search}
-        onChange={(e)=> dispatch({type: 'search', payload: e.target.value})}
-        
-        // https://www.npmjs.com/package/@react-google-maps/api
-        
-        />
-          </form>
+          <input
+            id="receipt-search"
+            type="text"
+            role="searchbox"
+            placeholder="Search by date"
+            value={state.search}
+            onChange={(e) =>
+              dispatch({ type: "search", payload: e.target.value })
+            }
 
+            // https://www.npmjs.com/package/@react-google-maps/api
+          />
+        </form>
 
-          {/* <SearchItem/> */}
-        </article>
-       
-            {state.getNames && state.getNames.map((item)=> {
-                console.log(state.getNames)
-                console.log(item.goods && item.goods)
-                // console.log(item)
-                return (
-                    <section
-                key={item._id}
-                    >
-                        <Link to="/one-receipt"
-                        style={{
-                            textDecoration: 'none',
-                        }}
-                        onClick={() => oneShow(item._id)}
-                        >
-                 <article
+        {/* <SearchItem/> */}
+      </article>
 
-                 id="receipts"
-                    >
-                        {/* <h5>cashierID: {item.cashierID}</h5> */}
-                         <h2 className="receipt-title">{bizName}</h2>
-                        <p>Date: {item.date}</p>
-                        <p>TransID: {item._id}</p>
-                        {item.goods && item.goods.map((good)=> {
-                            return (
-                                <div
-                                className="goods-container"
-                              
-                                >
-                                    <h4>{good.name}</h4>
-                                    <p>Qty: {good.qty}{good.unitMeasure.split(' ')[1].slice(1, -1)}</p>
-                                    <p>Price: {good.price}</p>
-                                    <p
-                                   
-                                    >Sub Total: ₦{numberWithCommas(parseFloat(good.total).toFixed(2))}</p>
-                               
-                                    {/* <br/> */}
-                                </div>
-                            )
-                        })}
-                     
-                        <h4
-                        className="receipts-grand-total"
-                        
-                        >Grand Total: ₦{ numberWithCommas(parseFloat(item.grandTotal).toFixed(2))}</h4>
-                        
-                   
-           <h5>Cashier: {item.cashier}</h5>
-                     
-                    </article>
-                    </Link>
-                    {/* <h3 onClick={(id)=> handleRemove(item._id)} */}
-                    <h3 onClick={(e)=> assertain(item._id, e)}
-                            style={{
-                                textAlign: 'center',
-                            }}
-                            >
-                        <FaTrashAlt role='button'
-           
-           /> 
-                        </h3>
-                        <br/>
-                    </section>
-                )
-            //    })
-            })}
-            
-            <div
-             className={state.cancel ? 'delete' : 'no-delete'}
-         >
-             <h3
+      {state.getNames &&
+        state.getNames.map((item) => {
+          console.log(state.getNames);
+          console.log(item.goods && item.goods);
+          // console.log(item)
+          return (
+            <section key={item._id}>
+              <Link
+                to="/one-receipt"
+                style={{
+                  textDecoration: "none",
+                }}
+                onClick={() => oneShow(item._id)}
+              >
+                <article id="receipts">
+                  {/* <h5>cashierID: {item.cashierID}</h5> */}
+                  <h2 className="receipt-title">{bizName}</h2>
+                  <p>Date: {item.date}</p>
+                  <p>TransID: {item._id}</p>
+                  {item.goods &&
+                    item.goods.map((good) => {
+                      return (
+                        <div className="goods-container">
+                          <h4>{good.name}</h4>
+                          <p>
+                            Qty: {good.qty}
+                            {good.unitMeasure.split(" ")[1].slice(1, -1)}
+                          </p>
+                          <p>
+                            Price: {currency}
+                            {good.price}
+                          </p>
+                          <p>
+                            Sub Total: {currency}
+                            {numberWithCommas(
+                              parseFloat(good.total).toFixed(2),
+                            )}
+                          </p>
+
+                          {/* <br/> */}
+                        </div>
+                      );
+                    })}
+
+                  <h4 className="receipts-grand-total">
+                    Grand Total: {currency}
+                    {numberWithCommas(parseFloat(item.grandTotal).toFixed(2))}
+                  </h4>
+
+                  <h5>Cashier: {item.cashier}</h5>
+                </article>
+              </Link>
+              {/* <h3 onClick={(id)=> handleRemove(item._id)} */}
+              <h3
+                onClick={(e) => assertain(item._id, e)}
+                style={{
+                  textAlign: "center",
+                }}
+              >
+                <FaTrashAlt role="button" />
+              </h3>
+              <br />
+            </section>
+          );
+          //    })
+        })}
+
+      <div className={state.cancel ? "delete" : "no-delete"}>
+        <h3
           id="verify-header"
           style={{
-              margin: '.5rem auto',
+            margin: ".5rem auto",
             //   display: 'flex',
           }}
-          >Delete from Receipts</h3>
-                 <article
-                 className="delete-buttons"
-               
-                 >
-                    <button
-                 onClick={remainDelete}
-                 >No</button><button
-                  onClick={handleRemove}
-                 style={{backgroundColor: 'red',
-                     borderColor: 'red'
-                 }}
-                 >Yes</button></article></div> 
+        >
+          Delete from Receipts
+        </h3>
+        <article className="delete-buttons">
+          <button onClick={remainDelete}>No</button>
+          <button
+            onClick={handleRemove}
+            style={{ backgroundColor: "red", borderColor: "red" }}
+          >
+            Yes
+          </button>
+        </article>
+      </div>
 
-<div
- className={state.isMatched ? 'authorization-alert' : 'authorization'}
-     >
-         <h2
-      id="verify-header"
-      style={{
-          margin: '.5rem auto',
-        //   display: 'flex',
-      }}
-      >Unauthorized!</h2>
-      <button 
-      onClick={generalRemain}
-       >
-        ok</button>
-            </div> 
+      <div
+        className={state.isMatched ? "authorization-alert" : "authorization"}
+      >
+        <h2
+          id="verify-header"
+          style={{
+            margin: ".5rem auto",
+            //   display: 'flex',
+          }}
+        >
+          Unauthorized!
+        </h2>
+        <button onClick={generalRemain}>ok</button>
+      </div>
+    </div>
+  );
+};
 
-        </div>
-    )
-}
-
-export default Shopping
-
-
-
-
-
-
-
+export default Shopping;
